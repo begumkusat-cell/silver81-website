50318e9bf87bba1ac3d4aafa8593572ca252a70e
begüm kuşat <begumkusat@192.168.1.13>
2026-05-13 23:29:55 +0300

    Reduce Lost Child title and description size on mobile

--- app/stories/lost-child/page.tsx
+++ app/stories/lost-child/page.tsx
@@ -13,11 +13,11 @@ export default function LostChildPage() {
             Novel
           </p>
   
-          <h1 className="text-7xl mb-6">
+          <h1 className="text-5xl sm:text-7xl mb-6">
             The Lost Child
           </h1>
   
-          <p className="max-w-4xl text-xl leading-9 text-[#bfc5cc]/60 mb-16">
+          <p className="max-w-4xl text-lg sm:text-xl leading-9 text-[#bfc5cc]/60 mb-16">
           They say time heals all wounds, that the pain of the past fades away after a while. Viola doesn’t believe that any pain she has will heal with time, because she still carries the wounds of the past that cause her deep pain. She wants to escape from the place she had lived since birth, as she believed by escaping that place, she can escape everything.
           </p>
   
